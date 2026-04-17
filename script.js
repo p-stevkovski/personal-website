487d5be7394348d439284946318f3f95ae317661
Petar Stevkovski <113934019+p-stevkovski@users.noreply.github.com>
2026-04-18 01:35:18 +0200

    merge sites into one single page with inline js

--- script.js
+++
@@ -1,8 +0,0 @@
-const toggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelector('.nav-links');
-
-toggle.addEventListener('click', () => {
-    navLinks.classList.toggle('open');
-    toggle.classList.toggle('open');
-    toggle.textContent = toggle.classList.contains('open') ? '✕' : '☰';
-});
